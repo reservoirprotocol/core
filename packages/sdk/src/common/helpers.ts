5412667757b541e56f0e9d66799cd7acb936241d
Diego Ramos <ramosdiego@pm.me>
2022-01-12 12:00:02 -0500

    added documentation

--- packages/sdk/src/common/helpers.ts
+++ packages/sdk/src/common/helpers.ts
@@ -13,6 +13,9 @@ import Erc20Abi from "./abis/Erc20.json";
 import Erc721Abi from "./abis/Erc721.json";
 import Erc1155Abi from "./abis/Erc1155.json";
 
+/**
+ * The Erc20 interface provides partial functionality to interact with an ERC20 Ethereum smart contract.
+ */
 export class Erc20 {
   public contract: Contract;
 
@@ -20,6 +23,13 @@ export class Erc20 {
     this.contract = new Contract(address, Erc20Abi as any, provider);
   }
 
+  /**
+   * Sets amount as the allowance of spender over the caller’s tokens.
+   * @param approver Abstracted Ethereum Account, usually as a JsonRpcSigner
+   * @param spender Ethereum address of a contract allowed to spend the approver's tokens
+   * @param amount Token amount to be allowed to spend
+   * @returns The contract transaction
+   */
   public async approve(
     approver: Signer,
     spender: string,
@@ -28,10 +38,21 @@ export class Erc20 {
     return this.contract.connect(approver).approve(spender, amount);
   }
 
+  /**
+   * 
+   * @param owner The owner's Ethereum address
+   * @returns The owner's token balance
+   */
   public async getBalance(owner: string): Promise<BigNumberish> {
     return this.contract.balanceOf(owner);
   }
 
+  /**
+   * 
+   * @param owner Ethereum address to be queried
+   * @param spender Ethereum contract 
+   * @returns The remaining number of tokens that spender will be allowed to spend on behalf of owner through transferFrom.
+   */
   public async getAllowance(
     owner: string,
     spender: string
@@ -40,6 +61,9 @@ export class Erc20 {
   }
 }
 
+/**
+ * The Erc721 interface provides partial functionality to interact with an ERC721 Ethereum smart contract.
+ */
 export class Erc721 {
   public contract: Contract;
 
@@ -47,6 +71,12 @@ export class Erc721 {
     this.contract = new Contract(address, Erc721Abi as any, provider);
   }
 
+  /**
+   * Approve or remove operator as an operator for the caller. Operators can call transferFrom or safeTransferFrom for any token owned by the caller.
+   * @param approver Abstracted Ethereum Account as a JavaScript object, usually a JsonRpcSigner
+   * @param operator The operator's Ethereum address
+   * @returns The contract transaction
+   */
   public async approve(
     approver: Signer,
     operator: string
@@ -54,15 +84,29 @@ export class Erc721 {
     return this.contract.connect(approver).setApprovalForAll(operator, true);
   }
 
+  /**
+   * Returns the owner of a token 
+   * @param tokenId The token ID number
+   * @returns The token owner's Ethereum address
+   */
   public async getOwner(tokenId: BigNumberish): Promise<string> {
     return this.contract.ownerOf(tokenId);
   }
 
+  /**
+   * Determine if the operator is allowed to manage all of the assets of owner or not
+   * @param owner The owner's Ethereum address
+   * @param operator The operator's Ethereum address
+   * @returns Wether the operator is allowed to manage all of the assets of owner or not
+   */
   public async isApproved(owner: string, operator: string): Promise<boolean> {
     return this.contract.isApprovedForAll(owner, operator);
   }
 }
 
+/**
+ * The Erc1155 interface provides partial functionality to interact with an Erc1155 Ethereum smart contract.
+ */
 export class Erc1155 {
   public contract: Contract;
 
@@ -70,6 +114,12 @@ export class Erc1155 {
     this.contract = new Contract(address, Erc1155Abi as any, provider);
   }
 
+  /**
+   * Approve or remove operator as an operator for the caller. Operators can call transferFrom or safeTransferFrom for any token owned by the caller.
+   * @param approver Abstracted Ethereum Account, usually as a JsonRpcSigner
+   * @param operator The operator's Ethereum address
+   * @returns The contract transaction
+   */
   public async approve(
     approver: Signer,
     operator: string
@@ -77,6 +127,12 @@ export class Erc1155 {
     return this.contract.connect(approver).setApprovalForAll(operator, true);
   }
 
+  /**
+   * 
+   * @param owner The owner's Ethereum address
+   * @param tokenId The token ID number
+   * @returns The owner's token balance
+   */
   public async getBalance(
     owner: string,
     tokenId: BigNumberish
@@ -84,16 +140,32 @@ export class Erc1155 {
     return this.contract.balanceOf(owner, tokenId);
   }
 
+  /**
+   * Determine if the operator is allowed to manage all of the assets of owner or not
+   * @param owner The owner's Ethereum address
+   * @param operator The operator's Ethereum address
+   * @returns Wether the operator is allowed to manage all of the assets of owner or not
+   */
   public async isApproved(owner: string, operator: string): Promise<boolean> {
     return this.contract.isApprovedForAll(owner, operator);
   }
 }
 
+
+/**
+ * The Weth interface provides partial functionality to interact with the Wrapped ETH (WETH) Ethereum smart contract.
+ */
 export class Weth extends Erc20 {
   constructor(provider: Provider, chainId: number) {
     super(provider, Addresses.Weth[chainId]);
   }
 
+  /**
+   * Deposit ETH in the WETH smart contract to get the equivalent amount of WETH
+   * @param depositor Abstracted Ethereum account, usually as a JsonRpcSigner 
+   * @param amount ETH amount to be deposited in the WETH smart contract
+   * @returns The contract transaction
+   */
   public async deposit(
     depositor: Signer,
     amount: BigNumberish
--- packages/sdk/src/wyvern-v2/addresses.ts
+++ packages/sdk/src/wyvern-v2/addresses.ts
@@ -1,20 +1,32 @@
 // For now only Mainnet and Rinkeby are supported
 
+/**
+ * The Wyvern v2 exchange smart contract addresses 
+ */
 export const Exchange: any = {
   1: "0x7be8076f4ea4a4ad08075c2508e481d6c946d12b",
   4: "0x5206e78b21ce315ce284fb24cf05e0585a93b1d9",
 };
 
+/**
+ * The Wyvern v2 proxy registry smart contract addresses 
+ */
 export const ProxyRegistry: any = {
   1: "0xa5409ec958c83c3f309868babaca7c86dcb077c1",
   4: "0xf57b2c51ded3a29e6891aba85459d600256cf317",
 };
 
+/**
+ * The Wyvern v2 token transfer proxy smart contract addresses 
+ */
 export const TokenTransferProxy: any = {
   1: "0xe5c783ee536cf5e63e792988335c4255169be4e1",
   4: "0x82d102457854c985221249f86659c9d6cf12aa72",
 };
 
+/**
+ * The Wyvern v2 token range verifier smart contract addresses 
+ */
 export const TokenRangeVerifier: any = {
   1: "",
   4: "0x253C0d8F6AA4D89b57D5A0CaEa6131090Fd13cC5",
