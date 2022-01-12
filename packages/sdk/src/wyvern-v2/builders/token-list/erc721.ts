ed5787d9877b40ad842d45270bb2b956069bf250
George Roman <george.roman.99@gmail.com>
2022-01-12 19:29:37 +0100

    Expose merkle tree utilities from the token list builder

--- packages/sdk/src/wyvern-v2/builders/token-list/erc721.ts
+++ packages/sdk/src/wyvern-v2/builders/token-list/erc721.ts
@@ -1,9 +1,8 @@
 import { Interface, defaultAbiCoder } from "@ethersproject/abi";
 import { BigNumberish } from "@ethersproject/bignumber";
-import { AddressZero, HashZero } from "@ethersproject/constants";
-import { keccak256 } from "@ethersproject/solidity";
-import MerkleTree from "merkletreejs";
+import { AddressZero } from "@ethersproject/constants";
 
+import { generateMerkleProof, generateMerkleTree } from "./utils";
 import { BaseBuilder, BaseBuildParams } from "../base";
 import * as Addresses from "../../addresses";
 import { Order } from "../../order";
@@ -52,36 +51,6 @@ const REPLACEMENT_PATTERN_SELL = (numTokens: number) => {
     "0".repeat(128) +
     "0".repeat(64).repeat(numMerkleTreeLevels)
   );
-};
-
-const generateMerkleTree = (tokenIds: BigNumberish[]) => {
-  if (!tokenIds.length) {
-    throw new Error("Could not generate merkle tree");
-  }
-
-  const hashFn = (buffer: Buffer) =>
-    Buffer.from(keccak256(["bytes"], [buffer]).slice(2), "hex");
-  const leaves = tokenIds.map((tokenId) =>
-    Buffer.from(keccak256(["uint256"], [tokenId]).slice(2), "hex")
-  );
-  return new MerkleTree(leaves, hashFn, { sortPairs: true });
-};
-
-const generateMerkleProof = (merkleTree: MerkleTree, tokenId: BigNumberish) => {
-  const leaf = Buffer.from(keccak256(["uint256"], [tokenId]).slice(2), "hex");
-  const proof = merkleTree.getHexProof(leaf);
-  if (
-    "0x" + leaf.toString("hex") !== merkleTree.getHexRoot() &&
-    proof.length === 0
-  ) {
-    throw new Error("Could not generate merkle proof");
-  } else {
-    const numMerkleTreeLevels = merkleTree.getDepth();
-    while (proof.length < numMerkleTreeLevels) {
-      proof.push("0x" + "0".repeat(64));
-    }
-  }
-  return proof;
 };
 
 interface BuildParams extends BaseBuildParams {
