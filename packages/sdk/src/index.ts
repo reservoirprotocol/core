05fb1bffc68d4bb60c6030ada2d50091a29e163c
George Roman <george.roman.99@gmail.com>
2022-03-28 21:43:20 +0300

    feat: remove support for wyvern v2

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,6 +1,5 @@
 import * as Common from "./common";
 import * as LooksRare from "./looks-rare";
-import * as WyvernV2 from "./wyvern-v2";
 import * as WyvernV23 from "./wyvern-v2.3";
 
-export { Common, LooksRare, WyvernV2, WyvernV23 };
+export { Common, LooksRare, WyvernV23 };
--- packages/sdk/src/wyvern-v2/index.ts
+++
@@ -1,8 +0,0 @@
-import * as Addresses from "./addresses";
-import { Builders } from "./builders";
-import { Exchange } from "./exchange";
-import * as Helpers from "./helpers";
-import { Order } from "./order";
-import * as Types from "./types";
-
-export { Addresses, Builders, Exchange, Helpers, Order, Types };
