8b369ec200a7d16b8f013e2ca80757cd60cd8753
George Roman <george.roman.99@gmail.com>
2022-04-04 13:24:18 +0300

    feat: expose zeroex-v4 and opendao

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,6 +1,8 @@
 import * as Common from "./common";
 import * as LooksRare from "./looks-rare";
+import * as OpenDao from "./opendao";
 import * as WyvernV2 from "./wyvern-v2";
 import * as WyvernV23 from "./wyvern-v2.3";
+import * as ZeroExV4 from "./zeroex-v4";
 
-export { Common, LooksRare, WyvernV2, WyvernV23 };
+export { Common, LooksRare, OpenDao, WyvernV2, WyvernV23, ZeroExV4 };
---
+++ packages/sdk/src/opendao/addresses.ts
@@ -0,0 +1,9 @@
+// For now only Rinkeby is supported
+
+export const Exchange: any = {
+  4: "0xb446f2fddf9f2d7c0ad2a5c061f8a62223976ecc",
+};
+
+export const Eth: any = {
+  4: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+};
