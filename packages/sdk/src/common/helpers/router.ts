9d64f2ca5e107d9e0393ae51f58a053b9bb89071
George Roman <george.roman.99@gmail.com>
2022-05-13 09:48:55 +0300

    feat: update router abi

--- packages/sdk/src/common/helpers/router.ts
+++ packages/sdk/src/common/helpers/router.ts
@@ -1,18 +1,20 @@
 import { Provider } from "@ethersproject/abstract-provider";
 import { Contract } from "@ethersproject/contracts";
 
-import RouterV1Abi from "../abis/RouterV1.json";
+import RouterV2Abi from "../abis/RouterV2.json";
 
 export enum ROUTER_EXCHANGE_KIND {
   WYVERN_V23,
   LOOKS_RARE,
   ZEROEX_V4,
+  FOUNDATION,
+  X2Y2,
 }
 
 export class RouterV1 {
   public contract: Contract;
 
   constructor(provider: Provider, address: string) {
-    this.contract = new Contract(address, RouterV1Abi as any, provider);
+    this.contract = new Contract(address, RouterV2Abi as any, provider);
   }
 }
