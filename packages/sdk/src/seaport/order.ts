6dae8ab424610d412eca51366243cdd681210c49
George Roman <george.roman.99@gmail.com>
2022-06-13 14:47:22 +0300

    fix: include signature when normalizing order

--- packages/sdk/src/seaport/order.ts
+++ packages/sdk/src/seaport/order.ts
@@ -296,5 +296,6 @@ const normalize = (order: Types.OrderComponents): Types.OrderComponents => {
     salt: s(order.salt),
     conduitKey: lc(order.conduitKey),
     counter: s(order.counter),
+    signature: order.signature ? lc(order.signature) : undefined,
   };
 };
