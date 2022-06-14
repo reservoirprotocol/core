e07d3ebf8d134be8dd076068865701f866e3597b
George Roman <george.roman.99@gmail.com>
2022-06-14 09:21:27 +0300

    feat: add usdc address to common

--- packages/sdk/src/common/addresses.ts
+++ packages/sdk/src/common/addresses.ts
@@ -11,3 +11,8 @@ export const Weth: ChainIdToAddress = {
   1: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
   4: "0xc778417e063141139fce010982780140aa0cd5ab",
 };
+
+export const Usdc: ChainIdToAddress = {
+  1: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+  42: "0xb7a4f3e9097c08da09517b5ab877f7a917224ede",
+};
