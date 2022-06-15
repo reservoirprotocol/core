5519b3027dd82bc13b29cb14ebd696d1046ef7ac
George Roman <george.roman.99@gmail.com>
2022-06-15 16:21:59 +0300

    feat: add opensea pausable zone addresses

--- packages/sdk/src/seaport/addresses.ts
+++ packages/sdk/src/seaport/addresses.ts
@@ -9,3 +9,8 @@ export const ConduitController: ChainIdToAddress = {
   1: "0x00000000f9490004c11cef243f5400493c00ad63",
   4: "0x00000000f9490004c11cef243f5400493c00ad63",
 };
+
+export const PausableZone: ChainIdToAddress = {
+  1: "0x004c00500000ad104d7dbd00e3ae0a5c00560c00",
+  4: "0x00000000e88fe2628ebc5da81d2b3cead633e89e",
+};
