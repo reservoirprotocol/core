474f496932bfe63b9ca4a2cf127f66ad4619429c
Diego Ramos <ramosdiego@pm.me>
2022-08-03 11:50:17 -0400

    added zora contract addresses

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -9,6 +9,7 @@ import * as WyvernV23 from "./wyvern-v2.3";
 import * as X2Y2 from "./x2y2";
 import * as ZeroExV4 from "./zeroex-v4";
 import * as Element from "./element";
+import * as Zora from "./zora";
 
 import * as Router from "./router";
 
@@ -25,4 +26,5 @@ export {
   ZeroExV4,
   Router,
   Element,
+  Zora,
 };
