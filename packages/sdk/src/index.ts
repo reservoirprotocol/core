279e7a8f91c8cfeab3e05a26c61c494ad0b36b58
George Roman <george.roman.99@gmail.com>
2022-08-04 09:48:20 +0300

    feat: add quixotic seaport address

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,34 +1,36 @@
 import * as Common from "./common";
+import * as CryptoPunks from "./cryptopunks";
+import * as Element from "./element";
 import * as Foundation from "./foundation";
 import * as LooksRare from "./looks-rare";
 import * as OpenDao from "./opendao";
+import * as Quixotic from "./quixotic";
 import * as Rarible from "./rarible";
 import * as Seaport from "./seaport";
+import * as SuperRare from "./superrare";
 import * as WyvernV2 from "./wyvern-v2";
 import * as WyvernV23 from "./wyvern-v2.3";
 import * as X2Y2 from "./x2y2";
 import * as ZeroExV4 from "./zeroex-v4";
-import * as Element from "./element";
 import * as Zora from "./zora";
-import * as SuperRare from "./superrare";
-import * as CryptoPunks from "./cryptopunks";
 
 import * as Router from "./router";
 
 export {
   Common,
+  CryptoPunks,
+  Element,
   Foundation,
   LooksRare,
   OpenDao,
+  Quixotic,
   Rarible,
   Seaport,
+  SuperRare,
   WyvernV2,
   WyvernV23,
   X2Y2,
   ZeroExV4,
-  Router,
-  Element,
   Zora,
-  SuperRare,
-  CryptoPunks,
+  Router,
 };
---
+++ packages/sdk/src/quixotic/addresses.ts
@@ -0,0 +1,5 @@
+import { ChainIdToAddress, Network } from "../utils";
+
+export const Exchange: ChainIdToAddress = {
+  [Network.Optimism]: "0x998ef16ea4111094eb5ee72fc2c6f4e6e8647666",
+};
