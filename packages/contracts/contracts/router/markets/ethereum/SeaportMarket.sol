05e8605e118fdd33744d2e4cbd8b7c3df6f706d3
George Roman <george.roman.99@gmail.com>
2022-08-07 19:37:11 +0300

    feat: add support for fees-on-top

--- packages/contracts/contracts/router/markets/ethereum/SeaportMarket.sol
+++ packages/contracts/contracts/router/markets/ethereum/SeaportMarket.sol
@@ -10,6 +10,13 @@ import {ISeaport} from "../../interfaces/ISeaport.sol";
 contract SeaportMarket is BaseMarket {
     using SafeERC20 for IERC20;
 
+    // --- Structs ---
+
+    struct SeaportFulfillments {
+        ISeaport.FulfillmentComponent[][] offer;
+        ISeaport.FulfillmentComponent[][] consideration;
+    }
+
     // --- Fields ---
 
     address public immutable exchange =
@@ -19,73 +26,140 @@ contract SeaportMarket is BaseMarket {
 
     constructor(address router) BaseMarket(router) {}
 
-    // --- Fill listings ---
-
-    // TODO: Add support for fees on top
-    struct ListingParams {
-        address receiver;
-        address refundTo;
-        bool revertIfIncomplete;
-    }
+    // --- Single ETH listing ---
 
     function acceptETHListing(
         ISeaport.AdvancedOrder calldata order,
-        ListingParams calldata params
-    ) external payable nonReentrant {
-        bool success;
-        try
-            ISeaport(exchange).fulfillAdvancedOrder{value: msg.value}(
-                order,
-                new ISeaport.CriteriaResolver[](0),
-                bytes32(0),
-                params.receiver
-            )
-        returns (bool fulfilled) {
-            success = fulfilled;
-        } catch {}
-
-        if (params.revertIfIncomplete && !success) {
-            revert UnsuccessfulFill();
-        }
-
-        // Refund any leftover
-        uint256 leftover = address(this).balance;
-        if (leftover > 0) {
-            (success, ) = payable(params.refundTo).call{value: leftover}("");
-            if (!success) {
-                revert UnsuccessfulPayment();
-            }
-        }
+        ETHListingParams calldata params
+    ) external payable nonReentrant refundETHLeftover(params.refundTo) {
+        fillSingleOrder(
+            order,
+            params.fillTo,
+            params.revertIfIncomplete,
+            params.amount
+        );
     }
+
+    function acceptETHListingWithFees(
+        ISeaport.AdvancedOrder calldata order,
+        ETHListingParams calldata params,
+        Fee[] calldata fees
+    )
+        external
+        payable
+        nonReentrant
+        refundETHLeftover(params.refundTo)
+        chargeETHFees(fees, params.amount)
+    {
+        fillSingleOrder(
+            order,
+            params.fillTo,
+            params.revertIfIncomplete,
+            params.amount
+        );
+    }
+
+    // --- Single ERC20 listing ---
 
     function acceptERC20Listing(
         ISeaport.AdvancedOrder calldata order,
-        ListingParams calldata params,
-        address erc20Token
-    ) external nonReentrant {
-        IERC20(erc20Token).safeApprove(exchange, type(uint256).max);
+        ERC20ListingParams calldata params
+    ) external nonReentrant refundERC20Leftover(params.refundTo, params.token) {
+        IERC20(params.token).safeApprove(exchange, params.amount);
+        fillSingleOrder(order, params.fillTo, params.revertIfIncomplete, 0);
+    }
 
-        bool success;
-        try
-            ISeaport(exchange).fulfillAdvancedOrder(
-                order,
-                new ISeaport.CriteriaResolver[](0),
-                bytes32(0),
-                params.receiver
-            )
-        returns (bool fulfilled) {
-            success = fulfilled;
-        } catch {}
+    function acceptERC20ListingWithFees(
+        ISeaport.AdvancedOrder calldata order,
+        ERC20ListingParams calldata params,
+        Fee[] calldata fees
+    )
+        external
+        nonReentrant
+        refundERC20Leftover(params.refundTo, params.token)
+        chargeERC20Fees(fees, params.token, params.amount)
+    {
+        IERC20(params.token).safeApprove(exchange, params.amount);
+        fillSingleOrder(order, params.fillTo, params.revertIfIncomplete, 0);
+    }
 
-        if (params.revertIfIncomplete && !success) {
-            revert UnsuccessfulFill();
-        }
+    // --- Multiple ETH listings ---
 
-        // Refund any leftover
-        uint256 leftover = IERC20(erc20Token).balanceOf(address(this));
-        if (leftover > 0) {
-            IERC20(erc20Token).safeTransfer(params.refundTo, leftover);
-        }
+    function acceptMultipleETHListings(
+        ISeaport.AdvancedOrder[] calldata orders,
+        // Use `memory` instead of `calldata` to avoid `Stack too deep` errors
+        SeaportFulfillments memory fulfillments,
+        ETHListingParams calldata params
+    ) external payable nonReentrant refundETHLeftover(params.refundTo) {
+        fillMultipleOrders(
+            orders,
+            fulfillments,
+            params.fillTo,
+            params.revertIfIncomplete,
+            params.amount
+        );
+    }
+
+    function acceptMultipleETHListingsWithFees(
+        ISeaport.AdvancedOrder[] calldata orders,
+        // Use `memory` instead of `calldata` to avoid `Stack too deep` errors
+        SeaportFulfillments memory fulfillments,
+        ETHListingParams calldata params,
+        Fee[] calldata fees
+    )
+        external
+        payable
+        nonReentrant
+        refundETHLeftover(params.refundTo)
+        chargeETHFees(fees, params.amount)
+    {
+        fillMultipleOrders(
+            orders,
+            fulfillments,
+            params.fillTo,
+            params.revertIfIncomplete,
+            params.amount
+        );
+    }
+
+    // --- Multiple ERC20 listings ---
+
+    function acceptMultipleERC20Listings(
+        ISeaport.AdvancedOrder[] calldata orders,
+        // Use `memory` instead of `calldata` to avoid `Stack too deep` errors
+        SeaportFulfillments memory fulfillments,
+        ERC20ListingParams calldata params
+    ) external nonReentrant refundERC20Leftover(params.refundTo, params.token) {
+        IERC20(params.token).safeApprove(exchange, params.amount);
+        fillMultipleOrders(
+            orders,
+            fulfillments,
+            params.fillTo,
+            params.revertIfIncomplete,
+            0
+        );
+    }
+
+    function acceptMultipleERC20ListingsWithFees(
+        ISeaport.AdvancedOrder[] calldata orders,
+        // Use `memory` instead of `calldata` to avoid `Stack too deep` errors
+        SeaportFulfillments memory fulfillments,
+        ERC20ListingParams calldata params,
+        Fee[] calldata fees
+    )
+        external
+        nonReentrant
+        refundERC20Leftover(params.refundTo, params.token)
+        chargeERC20Fees(fees, params.token, params.amount)
+    {
+        IERC20(params.token).safeApprove(exchange, params.amount);
+        fillMultipleOrders(
+            orders,
+            fulfillments,
+            params.fillTo,
+            params.revertIfIncomplete,
+            0
+        );
     }
 
     // --- Generic handler (used for Seaport-based approvals) ---
@@ -97,81 +171,62 @@ contract SeaportMarket is BaseMarket {
         ISeaport(exchange).matchOrders(orders, fulfillments);
     }
 
-    // function buySingle(
-    //     ISeaport.AdvancedOrder calldata order,
-    //     address receiver,
-    //     bool revertIfIncomplete
-    // ) external payable nonReentrant refund {
-    //     bool success;
-    //     try
-    //         ISeaport(exchange).fulfillAdvancedOrder{value: msg.value}(
-    //             order,
-    //             new ISeaport.CriteriaResolver[](0),
-    //             bytes32(0),
-    //             receiver
-    //         )
-    //     returns (bool fulfilled) {
-    //         success = fulfilled;
-    //     } catch {
-    //         success = false;
-    //     }
+    // --- Internal ---
 
-    //     if (!success && revertIfIncomplete) {
-    //         revert UnsuccessfulFill();
-    //     }
-    // }
+    function fillSingleOrder(
+        ISeaport.AdvancedOrder calldata order,
+        address receiver,
+        bool revertIfIncomplete,
+        uint256 value
+    ) internal {
+        bool success;
+        try
+            ISeaport(exchange).fulfillAdvancedOrder{value: value}(
+                order,
+                new ISeaport.CriteriaResolver[](0),
+                bytes32(0),
+                receiver
+            )
+        returns (bool fulfilled) {
+            success = fulfilled;
+        } catch {}
 
-    // function buyMultiple(
-    //     ISeaport.AdvancedOrder[] calldata orders,
-    //     // Use `memory` instead of `calldata` to avoid `Stack too deep` errors
-    //     ISeaport.FulfillmentComponent[][] memory offerFulfillments,
-    //     ISeaport.FulfillmentComponent[][] memory considerationFulfillments,
-    //     address receiver,
-    //     bool revertIfIncomplete
-    // ) external payable nonReentrant refund {
-    //     (bool[] memory fulfilled, ) = ISeaport(exchange)
-    //         .fulfillAvailableAdvancedOrders{value: msg.value}(
-    //         orders,
-    //         new ISeaport.CriteriaResolver[](0),
-    //         offerFulfillments,
-    //         considerationFulfillments,
-    //         bytes32(0),
-    //         receiver,
-    //         // Assume at most 255 orders can be filled at once
-    //         0xff
-    //     );
+        if (revertIfIncomplete && !success) {
+            revert UnsuccessfulFill();
+        }
+    }
 
-    //     if (revertIfIncomplete) {
-    //         uint256 length = fulfilled.length;
-    //         for (uint256 i = 0; i < length; ) {
-    //             if (!fulfilled[i]) {
-    //                 revert UnsuccessfulFill();
-    //             }
-    //             unchecked {
-    //                 ++i;
-    //             }
-    //         }
-    //     }
-    // }
+    function fillMultipleOrders(
+        ISeaport.AdvancedOrder[] calldata orders,
+        // Use `memory` instead of `calldata` to avoid `Stack too deep` errors
+        SeaportFulfillments memory fulfillments,
+        address receiver,
+        bool revertIfIncomplete,
+        uint256 value
+    ) internal {
+        (bool[] memory fulfilled, ) = ISeaport(exchange)
+            .fulfillAvailableAdvancedOrders{value: value}(
+            orders,
+            new ISeaport.CriteriaResolver[](0),
+            fulfillments.offer,
+            fulfillments.consideration,
+            bytes32(0),
+            receiver,
+            // Assume at most 255 orders can be filled at once
+            0xff
+        );
 
-    // // --- Fill bids ---
+        if (revertIfIncomplete) {
+            uint256 length = fulfilled.length;
+            for (uint256 i = 0; i < length; ) {
+                if (!fulfilled[i]) {
+                    revert UnsuccessfulFill();
+                }
 
-    // function sellSingle(ISeaport.AdvancedOrder calldata order, address receiver)
-    //     external
-    //     nonReentrant
-    // {
-    //     bool fulfilled = ISeaport(exchange).fulfillAdvancedOrder(
-    //         order,
-    //         new ISeaport.CriteriaResolver[](0),
-    //         bytes32(0),
-    //         receiver
-    //     );
-    //     if (!fulfilled) {
-    //         revert UnsuccessfulFill();
-    //     }
-    // }
-
-    // --- ERC721 / ERC1155 hooks ---
-
-    // Not needed since Seaport will send any NFTs directly to the receiver
+                unchecked {
+                    ++i;
+                }
+            }
+        }
+    }
 }
