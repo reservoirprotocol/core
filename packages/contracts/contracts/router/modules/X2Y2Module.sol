de9a8ac11a1253d3b850e1b069ad50330ed9ae91
George Roman <george.roman.99@gmail.com>
2022-08-10 16:17:44 +0300

    feat: add x2y2 module

---
+++ packages/contracts/contracts/router/modules/X2Y2Module.sol
@@ -0,0 +1,117 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+import {BaseModule} from "./BaseModule.sol";
+import {IX2Y2} from "../interfaces/IX2Y2.sol";
+
+contract X2Y2Module is BaseModule {
+    using SafeERC20 for IERC20;
+
+    // --- Fields ---
+
+    address public immutable exchange =
+        0x74312363e45DCaBA76c59ec49a7Aa8A65a67EeD3;
+
+    // --- Constructor ---
+
+    constructor(address router) BaseModule(router) {}
+
+    // --- [ERC721] Single ETH listing ---
+
+    function acceptETHListing(
+        IX2Y2.RunInput calldata input,
+        ETHListingParams calldata params,
+        Fee[] calldata fees
+    )
+        external
+        payable
+        nonReentrant
+        refundETHLeftover(params.refundTo)
+        chargeETHFees(fees, params.amount)
+    {
+        buyERC721(
+            input,
+            params.fillTo,
+            params.revertIfIncomplete,
+            params.amount
+        );
+    }
+
+    // --- [ERC721] Single ERC20 listing ---
+
+    function acceptERC20Listing(
+        IX2Y2.RunInput calldata input,
+        ERC20ListingParams calldata params,
+        Fee[] calldata fees
+    )
+        external
+        nonReentrant
+        refundERC20Leftover(params.refundTo, params.token)
+        chargeERC20Fees(fees, params.token, params.amount)
+    {
+        IERC20(params.token).safeApprove(exchange, params.amount);
+        buyERC721(input, params.fillTo, params.revertIfIncomplete, 0);
+    }
+
+    // --- Internal ---
+
+    function buyERC721(
+        IX2Y2.RunInput calldata input,
+        address receiver,
+        bool revertIfIncomplete,
+        uint256 value
+    ) internal {
+        if (input.details.length != 1) {
+            revert WrongParams();
+        }
+
+        IX2Y2.SettleDetail calldata detail = input.details[0];
+        IX2Y2.OrderItem calldata orderItem = input
+            .orders[detail.orderIdx]
+            .items[detail.itemIdx];
+        IX2Y2.Pair[] memory pairs = abi.decode(orderItem.data, (IX2Y2.Pair[]));
+        if (pairs.length != 1) {
+            revert WrongParams();
+        }
+
+        bool success;
+        try IX2Y2(exchange).run{value: value}(input) {
+            IERC721(pairs[0].token).safeTransferFrom(
+                address(this),
+                receiver,
+                pairs[0].tokenId
+            );
+
+            success = true;
+        } catch {}
+
+        if (revertIfIncomplete && !success) {
+            revert UnsuccessfulFill();
+        }
+    }
+
+    // --- ERC721 / ERC1155 hooks ---
+
+    function onERC721Received(
+        address, // operator,
+        address, // from
+        uint256, // tokenId,
+        bytes calldata // data
+    ) external pure returns (bytes4) {
+        return this.onERC721Received.selector;
+    }
+
+    function onERC1155Received(
+        address, // operator
+        address, // from
+        uint256, // tokenId
+        uint256, // amount
+        bytes calldata // data
+    ) external pure returns (bytes4) {
+        return this.onERC1155Received.selector;
+    }
+}
