a52324843025aa826544c4bf23b6a625f1bbd5c4
Diego Ramos <ramosdiego@pm.me>
2022-08-11 12:05:15 -0400

    updated nouns contract addresses

--- packages/sdk/src/nouns/addresses.ts
+++ packages/sdk/src/nouns/addresses.ts
@@ -1,5 +1,9 @@
 import { ChainIdToAddress, Network } from "../utils";
 
-export const Exchange: ChainIdToAddress = {
+export const AuctionHouse: ChainIdToAddress = {
   [Network.Ethereum]: "0x830bd73e4184cef73443c15111a1df14e495c706",
+};
+
+export const TokenContract: ChainIdToAddress = {
+  [Network.Ethereum]: "0x9c8ff314c9bc7f6e59a9d9225fb22946427edc03",
 };
