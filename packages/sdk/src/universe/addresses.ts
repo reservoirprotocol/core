4b391b103f721ccebd04a8fb7df4e2a1f36b8e8a
Ivo <ivostoynovski@gmail.com>
2022-08-30 17:58:15 +0300

    Add Universe address

---
+++ packages/sdk/src/universe/addresses.ts
@@ -0,0 +1,6 @@
+import { ChainIdToAddress, Network } from "../utils";
+
+export const Exchange: ChainIdToAddress = {
+  [Network.Ethereum]: "0xB720279a6fc982721771427aA3B6B3a135f81dE3",
+  [Network.EthereumRinkeby]: "0xe441c394F917D5a90019C76897458257801ce132"
+};
---
+++ packages/sdk/src/universe/index.ts
@@ -0,0 +1,3 @@
+import * as Addresses from "./addresses";
+
+export { Addresses };
