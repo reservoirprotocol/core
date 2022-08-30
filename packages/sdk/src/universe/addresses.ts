16935ced42ad46294658c93830cec952a122a751
Ivo <ivostoynovski@gmail.com>
2022-08-30 17:58:15 +0300

    resolve PR comment regarding lowercase addresses

--- packages/sdk/src/universe/addresses.ts
+++ packages/sdk/src/universe/addresses.ts
@@ -1,6 +1,6 @@
 import { ChainIdToAddress, Network } from "../utils";
 
 export const Exchange: ChainIdToAddress = {
-  [Network.Ethereum]: "0xB720279a6fc982721771427aA3B6B3a135f81dE3",
-  [Network.EthereumRinkeby]: "0xe441c394F917D5a90019C76897458257801ce132"
+  [Network.Ethereum]: "0xb720279a6fc982721771427aa3b6b3a135f81de3",
+  [Network.EthereumRinkeby]: "0xe441c394f917d5a90019c76897458257801ce132"
 };
