99cc8953f49480283ad2cf9fb11683549d0652a9
George Roman <george.roman.99@gmail.com>
2022-09-06 15:29:16 +0300

    feat: add nftx

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -3,19 +3,20 @@ import * as CryptoPunks from "./cryptopunks";
 import * as Element from "./element";
 import * as Foundation from "./foundation";
 import * as LooksRare from "./looks-rare";
+import * as Nftx from "./nftx";
 import * as Nouns from "./nouns";
 import * as OpenDao from "./opendao";
 import * as Quixotic from "./quixotic";
 import * as Rarible from "./rarible";
 import * as Seaport from "./seaport";
+import * as Sudoswap from "./sudoswap";
 import * as SuperRare from "./superrare";
+import * as Universe from "./universe";
 import * as WyvernV2 from "./wyvern-v2";
 import * as WyvernV23 from "./wyvern-v2.3";
 import * as X2Y2 from "./x2y2";
 import * as ZeroExV4 from "./zeroex-v4";
 import * as Zora from "./zora";
-import * as Universe from "./universe";
-import * as Sudoswap from "./sudoswap";
 
 import * as Router from "./router";
 
@@ -25,18 +26,19 @@ export {
   Element,
   Foundation,
   LooksRare,
+  Nftx,
   Nouns,
   OpenDao,
   Quixotic,
   Rarible,
   Seaport,
+  Sudoswap,
   SuperRare,
+  Universe,
   WyvernV2,
   WyvernV23,
   X2Y2,
   ZeroExV4,
   Zora,
-  Universe,
-  Sudoswap,
   Router,
 };
---
+++ packages/sdk/src/nftx/addresses.ts
@@ -0,0 +1,5 @@
+import { ChainIdToAddress, Network } from "../utils";
+
+export const VaultFactory: ChainIdToAddress = {
+  [Network.Ethereum]: "0xbe86f647b167567525ccaafcd6f881f1ee558216",
+};
