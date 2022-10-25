35c0f39199357844fe212bbef9e417317405c141
smatthewenglish <s.matthew.english@gmail.com>
2022-10-25 17:39:27 -0400

    selling _into_

--- packages/contracts/test/sdk/sudoswap/single-token/erc721.test.ts
+++ packages/contracts/test/sdk/sudoswap/single-token/erc721.test.ts
@@ -24,18 +24,24 @@ describe("Foundation - SingleToken Erc721", () => {
 
   it("Default test", async () => {
 
+    let tokenId = 6113;
+
+    let address = "0xaCd1423E1e7D45DD0F3AE63C5dB959D49FeADd3F";
+    let abi = '[{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"}]';
+    let pudgyDickbutts = new ethers.Contract(address, abi, ethers.provider);
+
+    let owner00 = await pudgyDickbutts.ownerOf(tokenId);
+
     // @ts-ignore
     let pairFactory = new ethers.Contract(Addresses.PairFactory[1], PairFactoryAbi, ethers.provider); //selling/deposit
 
     let nft = "0xaCd1423E1e7D45DD0F3AE63C5dB959D49FeADd3F"; //PudgyDickbutts
-    let ids = [6113];
+    let ids = [tokenId];
     let recipient = "0x7794C476806731b74ba2049ccd413218248135DA"; //pool
 
     let data = pairFactory.interface.encodeFunctionData("depositNFTs", [
         nft, ids, recipient
     ]);
-
-    let owner00 = "0x18181a21BB74A9De56d1Fbd408c4FeC175Ca0b16";
 
     const tx = {
       from: owner00,
@@ -46,8 +52,8 @@ describe("Foundation - SingleToken Erc721", () => {
     const impersonatedSigner = await ethers.getImpersonatedSigner(owner00);
     await impersonatedSigner.sendTransaction(tx);
 
-    //let owner01 = await contract.ownerOf(tokenId);
+    let owner0x = await pudgyDickbutts.ownerOf(tokenId);
+    expect(owner0x).to.eq(recipient);
 
-    //expect(owner01).to.eq(bob.address);
   });
 });
