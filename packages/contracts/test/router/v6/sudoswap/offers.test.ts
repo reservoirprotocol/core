41fe15881abb3fea0f0617cb36fbe44c3ca4cd7e
smatthewenglish <s.matthew.english@gmail.com>
2022-10-26 18:26:11 -0400

    clean it up

--- packages/contracts/test/router/v6/sudoswap/offers.test.ts
+++ packages/contracts/test/router/v6/sudoswap/offers.test.ts
@@ -36,42 +36,11 @@ describe("[ReservoirV6_0_0] Sudoswap offers", () => {
       )) as any;
   });
 
-  const getBalances = async (token: string) => {
-    if (token === Sdk.Common.Addresses.Eth[chainId]) {
-      return {
-        alice: await ethers.provider.getBalance(alice.address),
-        bob: await ethers.provider.getBalance(bob.address),
-        carol: await ethers.provider.getBalance(carol.address),
-        david: await ethers.provider.getBalance(david.address),
-        emilio: await ethers.provider.getBalance(emilio.address),
-        router: await ethers.provider.getBalance(router.address),
-        sudoswapModule: await ethers.provider.getBalance(
-          sudoswapModule.address
-        ),
-      };
-    } else {
-      const contract = new Sdk.Common.Helpers.Erc20(ethers.provider, token);
-      return {
-        alice: await contract.getBalance(alice.address),
-        bob: await contract.getBalance(bob.address),
-        carol: await contract.getBalance(carol.address),
-        david: await contract.getBalance(david.address),
-        emilio: await contract.getBalance(emilio.address),
-        router: await contract.getBalance(router.address),
-        sudoswapModule: await contract.getBalance(sudoswapModule.address),
-      };
-    }
-  };
-
   /**
    * npx hardhat test test/router/v6/sudoswap/offers.test.ts
    */
   it("Sudoswap router test", async () => {
     // Setup
-
-    // Makers: Alice and Bob
-    // Taker: Carol
-    // Fee recipient: Emilio
 
     let tokenId = 6113;
 
@@ -89,7 +58,7 @@ describe("[ReservoirV6_0_0] Sudoswap offers", () => {
 
     const impersonatedSigner = await ethers.getImpersonatedSigner(owner00);
 
-    // list nft...
+    // List nft
 
     await pairFactory.depositNFTs(impersonatedSigner, nft, ids, recipient);
 
@@ -109,36 +78,16 @@ describe("[ReservoirV6_0_0] Sudoswap offers", () => {
 
     let execution: ExecutionInfo[] = [{module: module, data: data, value: value.toString()}];
 
-    // Fetch pre-state
-
-    const balancesBefore = await getBalances(Sdk.Common.Addresses.Eth[chainId]);
-
     // Execute
 
     await router.execute(execution, {
       value: parseEther("0.2")
     });
 
-    // Fetch post-state
-
-    const balancesAfter = await getBalances(Sdk.Common.Addresses.Eth[chainId]);
-
     // Checks
 
     let owner0y = await contractPDB.ownerOf(tokenId);
     expect(owner0y).to.eq(alice.address);
-
-    // Alice got the payment
-   
-    // Bob got the payment
-
-    // Emilio got the fee payments
-
-    // Carol got the NFTs from all filled orders
-
-    // Router is stateless
-    expect(balancesAfter.router).to.eq(0);
-    expect(balancesAfter.sudoswapModule).to.eq(0);
   });
 
 });
