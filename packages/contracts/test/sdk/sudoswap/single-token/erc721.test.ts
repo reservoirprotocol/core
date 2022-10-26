c5250a7cd047f5d9c3feedb59b07feb1b1516e23
smatthewenglish <s.matthew.english@gmail.com>
2022-10-26 12:27:17 -0400

    list/purchase

--- packages/contracts/test/sdk/sudoswap/single-token/erc721.test.ts
+++ packages/contracts/test/sdk/sudoswap/single-token/erc721.test.ts
@@ -1,6 +1,7 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { parseEther } from "@ethersproject/units";
 
 //import * as Sudoswap from "../../../../../sdk/src/sudoswap";
 import PairFactoryAbi from "../../../../../sdk/src/sudoswap/abis/FactoryPair.json";
@@ -20,17 +21,18 @@ describe("Foundation - SingleToken Erc721", () => {
     [deployer, alice, bob, carol] = await ethers.getSigners();
   });
 
-  //let pairRouter = new ethers.Contract(Addresses.PairRouter[1], PairRouterAbi, ethers.provider); //purchasing
-
+  /**
+   * npx hardhat test test/sdk/sudoswap/single-token/erc721.test.ts
+   */
   it("Default test", async () => {
 
     let tokenId = 6113;
 
-    let address = "0xaCd1423E1e7D45DD0F3AE63C5dB959D49FeADd3F";
-    let abi = '[{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"}]';
-    let pudgyDickbutts = new ethers.Contract(address, abi, ethers.provider);
+    let addresPDB = "0xaCd1423E1e7D45DD0F3AE63C5dB959D49FeADd3F";
+    let abiOwnerOf = '[{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"}]';
+    let contractPDB = new ethers.Contract(addresPDB, abiOwnerOf, ethers.provider);
 
-    let owner00 = await pudgyDickbutts.ownerOf(tokenId);
+    let owner00 = await contractPDB.ownerOf(tokenId);
 
     // @ts-ignore
     let pairFactory = new ethers.Contract(Addresses.PairFactory[1], PairFactoryAbi, ethers.provider); //selling/deposit
@@ -52,8 +54,33 @@ describe("Foundation - SingleToken Erc721", () => {
     const impersonatedSigner = await ethers.getImpersonatedSigner(owner00);
     await impersonatedSigner.sendTransaction(tx);
 
-    let owner0x = await pudgyDickbutts.ownerOf(tokenId);
+    let owner0x = await contractPDB.ownerOf(tokenId);
     expect(owner0x).to.eq(recipient);
 
+    // now buy it...
+
+    let pairRouter = new ethers.Contract(Addresses.PairRouter[1], PairRouterAbi, ethers.provider); //purchasing
+
+    let swapListPair = recipient;
+    let swapListNftIds = ids;
+    let ethRecipient =	bob.address;
+    let nftRecipient =	alice.address;
+    let deadline =	1666734514;
+
+    let data01 = pairRouter.interface.encodeFunctionData("swapETHForSpecificNFTs", [
+        [[swapListPair, swapListNftIds]], ethRecipient, nftRecipient, deadline
+    ]);
+
+    const tx01 = {
+        from: alice.address,
+        to: pairRouter.address,
+        data: data01,
+        value: parseEther("0.2")
+    };
+
+    await alice.sendTransaction(tx01);
+
+    let owner0y = await contractPDB.ownerOf(tokenId);
+    expect(owner0y).to.eq(alice.address);
   });
 });
