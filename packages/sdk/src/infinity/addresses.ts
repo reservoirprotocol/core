4958a81e36968ace1eec32185eeb370775b95483
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-11-08 10:47:29 -0600

    Added: Infinity Exchange addresses

---
+++ packages/sdk/src/infinity/addresses.ts
@@ -0,0 +1,7 @@
+import { ChainIdToAddress, Network } from "../utils";
+
+export const Exchange: ChainIdToAddress = {
+    [Network.Ethereum]: "0xbada5551b2f08d3959329b2ff8d0a7cc8be26324",
+    [Network.EthereumGoerli]: "0xf0b83ed51fa7c9617dd48fe5864566bbd9519e4b",
+    [Network.Polygon]: "0x5c600fff0ac90cdf026a16e4e315a2471f7bf7a6"
+}
