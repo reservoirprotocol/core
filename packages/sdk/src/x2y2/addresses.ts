4a7c8cf0cadb253d13a5ee0c0cdc8ffdf94b1b12
azianmike <michaeluo@gmail.com>
2022-11-08 19:36:24 -0800

    Add in x2y2 goerli addresses

--- packages/sdk/src/x2y2/addresses.ts
+++ packages/sdk/src/x2y2/addresses.ts
@@ -2,12 +2,15 @@ import { ChainIdToAddress, Network } from "../utils";
 
 export const Exchange: ChainIdToAddress = {
   [Network.Ethereum]: "0x74312363e45dcaba76c59ec49a7aa8a65a67eed3",
+  [Network.EthereumGoerli]: "0x1891EcD5F7b1E751151d857265D6e6D08ae8989e",
 };
 
 export const Erc721Delegate: ChainIdToAddress = {
   [Network.Ethereum]: "0xf849de01b080adc3a814fabe1e2087475cf2e354",
+  [Network.EthereumGoerli]: "0x095be13D86000260852E4F92eA48dc333fa35249",
 };
 
 export const FeeManager: ChainIdToAddress = {
   [Network.Ethereum]: "0xd823c605807cc5e6bd6fc0d7e4eea50d3e2d66cd",
+  [Network.EthereumGoerli]: "0x809eed76f38d7c62461f8c5ececce3a8224c1707",
 };
