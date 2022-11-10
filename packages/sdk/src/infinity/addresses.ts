45275d99920339968286facb5637a55e429979d1
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-11-10 12:00:19 -0600

    Added: Infinity Complication addresses

--- packages/sdk/src/infinity/addresses.ts
+++ packages/sdk/src/infinity/addresses.ts
@@ -5,3 +5,9 @@ export const Exchange: ChainIdToAddress = {
     [Network.EthereumGoerli]: "0xf0b83ed51fa7c9617dd48fe5864566bbd9519e4b",
     [Network.Polygon]: "0x5c600fff0ac90cdf026a16e4e315a2471f7bf7a6"
 }
+
+export const Complication: ChainIdToAddress = {
+    [Network.Ethereum]: "0xbada5555fe632ace2c90fee8c060703369c25f1c",
+    [Network.EthereumGoerli]: "0x6deb5e1a056975e0f2024f3d89b6d2465bde22af",
+    [Network.Polygon]: "0x748c74994fff570d7e3fd14f25c17c3d9702832c"
+}
