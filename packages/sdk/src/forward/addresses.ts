861f0f4c948bcff1851c4218e258d4afee6a1784
George Roman <george.roman.99@gmail.com>
2022-11-14 15:39:19 +0100

    chore: upgrade forward address on goerli

--- packages/sdk/src/forward/addresses.ts
+++ packages/sdk/src/forward/addresses.ts
@@ -1,5 +1,5 @@
 import { ChainIdToAddress, Network } from "../utils";
 
 export const Exchange: ChainIdToAddress = {
-  [Network.EthereumGoerli]: "0x0f68e10295de83a22de689bc7cec4fb6d8cec25f",
+  [Network.EthereumGoerli]: "0x5f78d53122f88f3abbed88090e1648b5f9f824bd",
 };
