7ad2acf056beca94c5d56883510d34df67870ddd
George Roman <george.roman.99@gmail.com>
2022-11-22 18:28:38 +0100

    fix: build

--- packages/sdk/src/sudoswap/addresses.ts
+++ packages/sdk/src/sudoswap/addresses.ts
@@ -4,6 +4,10 @@ export const PairFactory: ChainIdToAddress = {
   [Network.Ethereum]: "0xb16c1342e617a5b6e4b631eb114483fdb289c0a4",
 };
 
+export const RouterWithRoyalties: ChainIdToAddress = {
+  [Network.Ethereum]: "0x844d04f79d2c58dcebf8fff1e389fccb1401aa49",
+};
+
 export const LinearCurve: ChainIdToAddress = {
   [Network.Ethereum]: "0x5b6ac51d9b1cede0068a1b26533cace807f883ee",
 };
