ffd7418c9cab568b3708239a45ff3cf27b2363e5
George Roman <george.roman.99@gmail.com>
2022-11-22 18:23:50 +0100

    chore: redeploy some modules on goerli

--- packages/sdk/src/router/v6/addresses.ts
+++ packages/sdk/src/router/v6/addresses.ts
@@ -17,24 +17,24 @@ export const FoundationModule: ChainIdToAddress = {};
 
 export const LooksRareModule: ChainIdToAddress = {
   [Network.Ethereum]: "0x920692834f93258b71221c58edf870ae013e2f9b",
-  [Network.EthereumGoerli]: "0xe4c1c635f257348205ebca78fc9b342dd7813e2b",
+  [Network.EthereumGoerli]: "0x532486bb46581b032134159c1d31962cdab1e6a7",
 };
 
 export const SeaportModule: ChainIdToAddress = {
   [Network.Ethereum]: "0xef21d6b43ac0bb4608ca05628b05403a47310a3b",
-  [Network.EthereumGoerli]: "0x037d39e603b803651acc7b36ff25e52f8680aa2f",
+  [Network.EthereumGoerli]: "0x6c460f133c573c21e7f55900d0c68f6f085b91e7",
 };
 
 export const SudoswapModule: ChainIdToAddress = {};
 
 export const X2Y2Module: ChainIdToAddress = {
   [Network.Ethereum]: "0xff78f7c6e23187fd4bdb2f7f35359a42d56878dd",
-  [Network.EthereumGoerli]: "0x3b74ec4005617ce2adf46d812039ea3562a4eaf5",
+  [Network.EthereumGoerli]: "0x6a789513b2e555f9d3539bf9a053a57d2bfca426",
 };
 
 export const ZeroExV4Module: ChainIdToAddress = {
   [Network.Ethereum]: "0xca0aedd0dde85d059e41e2c4aed711732b5df844",
-  [Network.EthereumGoerli]: "0x0e01862920bd5ef73ed1a5dccd2ecad56c3e051f",
+  [Network.EthereumGoerli]: "0x29fcac61d9b2a3c55f3e1149d0278126c31abe74",
 };
 
 export const ZoraModule: ChainIdToAddress = {};
