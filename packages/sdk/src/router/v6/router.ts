f726b0b92a840b97d8e1a3bd7592f22e754a8503
George Roman <george.roman.99@gmail.com>
2022-11-24 08:41:15 +0100

    fix: skip zero amounts and recipients when paying fees

--- packages/sdk/src/router/v6/router.ts
+++ packages/sdk/src/router/v6/router.ts
@@ -315,15 +315,27 @@ export class Router {
 
     const getFees = (ownDetails: ListingFillDetails[]) => [
       // Global fees
-      ...(options?.globalFees ?? []).map(({ recipient, amount }) => ({
-        recipient,
-        // The fees are averaged over the number of listings to fill
-        // TODO: Also take into account the quantity filled for ERC1155
-        amount: bn(amount).mul(ownDetails.length).div(details.length),
-      })),
+      ...(options?.globalFees ?? [])
+        .filter(
+          ({ amount, recipient }) =>
+            // Skip zero amounts and/or recipients
+            bn(amount).gt(0) && recipient !== AddressZero
+        )
+        .map(({ recipient, amount }) => ({
+          recipient,
+          // The fees are averaged over the number of listings to fill
+          // TODO: Also take into account the quantity filled for ERC1155
+          amount: bn(amount).mul(ownDetails.length).div(details.length),
+        })),
       // Local fees
       // TODO: Should not split the local fees among all executions
-      ...ownDetails.flatMap(({ fees }) => fees ?? []),
+      ...ownDetails.flatMap(({ fees }) =>
+        (fees ?? []).filter(
+          ({ amount, recipient }) =>
+            // Skip zero amounts and/or recipients
+            bn(amount).gt(0) && recipient !== AddressZero
+        )
+      ),
     ];
 
     // For keeping track of the listing's position in the original array
