a3364ff55c6c4bbd5a9660127c5c5374abbf10b4
vikinatora <vikinatora@gmail.com>
2022-12-05 11:42:29 +0200

    feat: blur oracle sign and signature validation

--- packages/sdk/src/blur/addresses.ts
+++ packages/sdk/src/blur/addresses.ts
@@ -10,6 +10,10 @@ export const StandardPolicyERC721: ChainIdToAddress = {
   [Network.EthereumGoerli]: "0xde0cdfc21ad03afe917efa4e8811fc3fb4e8f027",
 };
 
+export const StandardPolicyERC721_V2: ChainIdToAddress = {
+  [Network.Ethereum]: "0x0000000000dab4a563819e8fd93dba3b25bc3495"
+};
+
 export const StandardPolicyERC1155: ChainIdToAddress = {
   [Network.EthereumGoerli]: "0x4146c41a880b0f1f1b22f31c6354f23a7f6be8b2",
 };
@@ -27,4 +31,9 @@ export const MerkleVerifier: ChainIdToAddress = {
 export const PolicyManager: ChainIdToAddress = {
   [Network.Ethereum]: "0x3a35a3102b5c6bd1e4d3237248be071ef53c8331",
   [Network.EthereumGoerli]: "0xc73c52c261ba61a222618a9ebc27ae79a18377d9"
+}
+
+export const Oracle: ChainIdToAddress = {
+  [Network.Ethereum]: "0xd44feab097e5fee7fd54c9481f56b806f7f3ddc1",
+  [Network.EthereumGoerli]: "0xe11816134ddd8728752995ee36e3b3cc72873166"
 }
--- packages/sdk/src/blur/order.ts
+++ packages/sdk/src/blur/order.ts
@@ -1,6 +1,6 @@
 import { Provider } from "@ethersproject/abstract-provider";
 import { TypedDataSigner } from "@ethersproject/abstract-signer";
-import { splitSignature } from "@ethersproject/bytes";
+import { splitSignature, Signature } from "@ethersproject/bytes";
 import { HashZero } from "@ethersproject/constants";
 import { Contract } from "@ethersproject/contracts";
 import { _TypedDataEncoder } from "@ethersproject/hash";
@@ -13,7 +13,7 @@ import * as Types from "./types";
 import * as Common from "../common";
 import { bn, lc, n, s, BytesEmpty } from "../utils";
 import ExchangeAbi from "./abis/Exchange.json";
-import { MerkleTree } from 'merkletreejs';
+import { MerkleTree } from "merkletreejs";
 import { keccak256, defaultAbiCoder } from "ethers/lib/utils";
 
 export class Order {
@@ -41,10 +41,10 @@ export class Order {
       v: this.params.v,
       r: this.params.r ?? "",
       s: this.params.s ?? "",
-      extraSignature:this.params.extraSignature ?? BytesEmpty,
+      extraSignature: this.params.extraSignature ?? BytesEmpty,
       signatureVersion: this.params.signatureVersion ?? 0,
-      blockNumber: this.params.blockNumber ?? 0
-    }
+      blockNumber: this.params.blockNumber ?? 0,
+    };
   }
 
   public hash() {
@@ -68,14 +68,43 @@ export class Order {
     };
   }
 
+  static async doOracleSign(order: Order, oracle: TypedDataSigner) {
+    const { types, value } = Order.getEip712TypesAndValueForOracle(order);
+    return splitSignature(
+      await oracle._signTypedData(EIP712_DOMAIN(order.chainId), types, value)
+    );
+  }
+
+  static getEip712TypesAndValueForOracle(order: Order) {
+    return {
+      types: ORACLE_ORDER_EIP712_TYPES,
+      value: {
+        order: toRawOrder(order),
+        blockNumber: order.params.blockNumber,
+      },
+    };
+  }
+
+  public async oracleSign(oracle: TypedDataSigner) {
+    const signature = await Order.doOracleSign(this, oracle);
+    this.params = {
+      ...this.params,
+      extraSignature: packSignature(signature),
+      extraParams: "0x01",
+    };
+  }
+
   static async signBulk(orders: Order[], signer: TypedDataSigner) {
     const { tree, root } = await getOrderTreeRoot(orders);
     const firstOrder = orders[0];
     const { v, r, s } = splitSignature(
-      await signer._signTypedData(EIP712_DOMAIN(firstOrder.chainId), 
-      ORDER_ROOT_EIP712_TYPES, {
-        root: root
-      })
+      await signer._signTypedData(
+        EIP712_DOMAIN(firstOrder.chainId),
+        ORDER_ROOT_EIP712_TYPES,
+        {
+          root: root,
+        }
+      )
     );
 
     // sign each order
@@ -83,15 +112,32 @@ export class Order {
       const order = orders[index];
       const orderHash = order.hash();
       const extraSignature = defaultAbiCoder.encode(
-        ['bytes32[]'],
-        [tree.getHexProof(orderHash)],
-      )
+        ["bytes32[]"],
+        [tree.getHexProof(orderHash)]
+      );
       order.params.extraSignature = extraSignature;
       // bulk
       order.params.signatureVersion = 1;
       order.params.r = r;
       order.params.v = v;
       order.params.s = s;
+    }
+  }
+
+  static async signBulkOracle(orders: Order[], oracle: TypedDataSigner) {
+    for (let index = 0; index < orders.length; index++) {
+      const order = orders[index];
+      const oracleSig = await Order.doOracleSign(order, oracle);
+      const extraSignature = defaultAbiCoder.encode(
+        ["bytes32[]", "uint8", "bytes32", "bytes32"],
+        [
+          defaultAbiCoder.decode(["bytes32[]"], order.params.extraSignature)[0],
+          oracleSig.v,
+          oracleSig.r,
+          oracleSig.s,
+        ]
+      );
+      order.params.extraSignature = extraSignature;
     }
   }
 
@@ -105,31 +151,80 @@ export class Order {
     };
   }
 
+  public isOracleSign() {
+    return this.params.extraParams.startsWith("0x01");
+  }
+
   public checkSignature() {
     const [types, value] = this.getEip712TypesAndValue();
+    const isOracleSign = this.isOracleSign();
     let signer: string | null;
     const signature = {
       v: this.params.v,
       r: this.params.r ?? "",
       s: this.params.s ?? "",
-    }
+    };
 
     // bulk sign
     if (this.params.signatureVersion === 1) {
-      const proof = defaultAbiCoder.decode(['bytes32[]'], this.params.extraSignature)[0];
+      const proof = defaultAbiCoder.decode(
+        ["bytes32[]"],
+        this.params.extraSignature
+      )[0];
       const tree = new MerkleTree([], keccak256, {
-        sort: true 
+        sort: true,
       });
       const treeRoot = computedRoot(tree, proof, this.hash());
-      signer = verifyTypedData(EIP712_DOMAIN(this.chainId), ORDER_ROOT_EIP712_TYPES, {
-        root: treeRoot
-      }, signature)
+      signer = verifyTypedData(
+        EIP712_DOMAIN(this.chainId),
+        ORDER_ROOT_EIP712_TYPES,
+        {
+          root: treeRoot,
+        },
+        signature
+      );
     } else {
-      signer = verifyTypedData(EIP712_DOMAIN(this.chainId), types, value, signature);
+      signer = verifyTypedData(
+        EIP712_DOMAIN(this.chainId),
+        types,
+        value,
+        signature
+      );
     }
 
     if (lc(this.params.trader) !== lc(signer)) {
       throw new Error("Invalid signature");
+    }
+
+    if (isOracleSign) {
+      // bulk sign
+      const { types, value } = Order.getEip712TypesAndValueForOracle(this);
+      if (this.params.signatureVersion === 1) {
+        const [merklePath, _v, _r, _s] = defaultAbiCoder.decode(
+          ["bytes32[]", "uint8", "bytes32", "bytes32"],
+          this.params.extraSignature
+        );
+        signer = verifyTypedData(EIP712_DOMAIN(this.chainId), types, value, {
+          v: _v,
+          r: _r,
+          s: _s,
+        });
+      } else {
+        const { types, value } = Order.getEip712TypesAndValueForOracle(this);
+        const [_v, _r, _s] = defaultAbiCoder.decode(
+          ["uint8", "bytes32", "bytes32"],
+          this.params.extraSignature
+        );
+        signer = verifyTypedData(EIP712_DOMAIN(this.chainId), types, value, {
+          v: _v,
+          r: _r,
+          s: _s,
+        });
+      }
+
+      if (Addresses.Oracle[this.chainId] !== lc(signer)) {
+        throw new Error("Invalid oracle signature");
+      }
     }
   }
 
@@ -159,7 +254,10 @@ export class Order {
     if (this.params.side === Types.TradeDirection.BUY) {
       // Check that maker has enough balance to cover the payment
       // and the approval to the token transfer proxy is set
-      const erc20 = new Common.Helpers.Erc20(provider, this.params.paymentToken);
+      const erc20 = new Common.Helpers.Erc20(
+        provider,
+        this.params.paymentToken
+      );
       const balance = await erc20.getBalance(this.params.trader);
       if (bn(balance).lt(bn(this.params.price))) {
         throw new Error("no-balance");
@@ -175,7 +273,10 @@ export class Order {
       }
     } else {
       if (this.params.kind?.startsWith("erc721")) {
-        const erc721 = new Common.Helpers.Erc721(provider, this.params.collection);
+        const erc721 = new Common.Helpers.Erc721(
+          provider,
+          this.params.collection
+        );
 
         // Check ownership
         const owner = await erc721.getOwner(this.params.tokenId);
@@ -192,7 +293,10 @@ export class Order {
           throw new Error("no-approval");
         }
       } else {
-        const erc1155 = new Common.Helpers.Erc1155(provider, this.params.collection);
+        const erc1155 = new Common.Helpers.Erc1155(
+          provider,
+          this.params.collection
+        );
 
         // Check balance
         const balance = await erc1155.getBalance(
@@ -230,9 +334,20 @@ export class Order {
   }
 
   private detectKind(): Types.OrderKind {
-    if (this.params.matchingPolicy === Addresses.StandardPolicyERC721[this.chainId]) {
-      return 'erc721-single-token'
+    if (
+      this.params.matchingPolicy ===
+      Addresses.StandardPolicyERC721[this.chainId]
+    ) {
+      return "erc721-single-token";
     }
+
+    if (
+      this.params.matchingPolicy ===
+      Addresses.StandardPolicyERC721_V2[this.chainId]
+    ) {
+      return "erc721-single-token";
+    }
+
     throw new Error(
       "Could not detect order kind (order might have unsupported params/calldata)"
     );
@@ -248,47 +363,80 @@ const EIP712_DOMAIN = (chainId: number) => ({
 
 const ORDER_EIP712_TYPES = {
   Order: [
-    { name: 'trader', type: 'address' },
-    { name: 'side', type: 'uint8' },
-    { name: 'matchingPolicy', type: 'address' },
-    { name: 'collection', type: 'address' },
-    { name: 'tokenId', type: 'uint256' },
-    { name: 'amount', type: 'uint256' },
-    { name: 'paymentToken', type: 'address' },
-    { name: 'price', type: 'uint256' },
-    { name: 'listingTime', type: 'uint256' },
-    { name: 'expirationTime', type: 'uint256' },
-    { name: 'fees', type: 'Fee[]' },
-    { name: 'salt', type: 'uint256' },
-    { name: 'extraParams', type: 'bytes' },
-    { name: 'nonce', type: 'uint256' },
+    { name: "trader", type: "address" },
+    { name: "side", type: "uint8" },
+    { name: "matchingPolicy", type: "address" },
+    { name: "collection", type: "address" },
+    { name: "tokenId", type: "uint256" },
+    { name: "amount", type: "uint256" },
+    { name: "paymentToken", type: "address" },
+    { name: "price", type: "uint256" },
+    { name: "listingTime", type: "uint256" },
+    { name: "expirationTime", type: "uint256" },
+    { name: "fees", type: "Fee[]" },
+    { name: "salt", type: "uint256" },
+    { name: "extraParams", type: "bytes" },
+    { name: "nonce", type: "uint256" },
   ],
   Fee: [
-    { name: 'rate', type: 'uint16' },
-    { name: 'recipient', type: 'address' },
-  ]
+    { name: "rate", type: "uint16" },
+    { name: "recipient", type: "address" },
+  ],
 };
 
+const ORACLE_ORDER_EIP712_TYPES = {
+  OracleOrder: [
+    { name: "order", type: "Order" },
+    { name: "blockNumber", type: "uint256" },
+  ],
+  Order: [
+    { name: "trader", type: "address" },
+    { name: "side", type: "uint8" },
+    { name: "matchingPolicy", type: "address" },
+    { name: "collection", type: "address" },
+    { name: "tokenId", type: "uint256" },
+    { name: "amount", type: "uint256" },
+    { name: "paymentToken", type: "address" },
+    { name: "price", type: "uint256" },
+    { name: "listingTime", type: "uint256" },
+    { name: "expirationTime", type: "uint256" },
+    { name: "fees", type: "Fee[]" },
+    { name: "salt", type: "uint256" },
+    { name: "extraParams", type: "bytes" },
+    { name: "nonce", type: "uint256" },
+  ],
+  Fee: [
+    { name: "rate", type: "uint16" },
+    { name: "recipient", type: "address" },
+  ],
+};
 
 const ORDER_ROOT_EIP712_TYPES = {
-  Root: [{ name: 'root', type: 'bytes32' }],
-}
+  Root: [{ name: "root", type: "bytes32" }],
+};
 
 const toRawOrder = (order: Order): any => ({
-  ...order.params
+  ...order.params,
 });
 
 function getMerkleProof(leaves: string[]) {
   const tree = new MerkleTree(leaves, keccak256, { sort: true });
   const root = tree.getHexRoot();
-  return { root, tree};
+  return { root, tree };
+}
+
+function packSignature(signature: Signature): string {
+  return defaultAbiCoder.encode(
+    ["uint8", "bytes32", "bytes32"],
+    [signature.v, signature.r, signature.s]
+  );
 }
 
 async function getOrderTreeRoot(orders: Order[]) {
   const leaves = await Promise.all(
     orders.map(async (order) => {
       return order.hash();
-    }),
+    })
   );
   return getMerkleProof(leaves);
 }
@@ -297,26 +445,25 @@ function computedRoot(tree: MerkleTree, proof: string[], targetNode: string) {
   const hashFn = tree.bufferifyFn(keccak256);
   let hash = tree.bufferify(targetNode);
   for (let i = 0; i < proof.length; i++) {
-      const node = proof[i];
-      let data = null;
-      let isLeftNode = null;
-      if (typeof node === 'string') {
-          data = tree.bufferify(node);
-          isLeftNode = true;
-      } else {
-        throw new Error('Expected node to be of type string or object');
-      }
-      const buffers = [];
-      if (Buffer.compare(hash, data) === -1) {
-        buffers.push(hash, data);
-        hash = hashFn(Buffer.concat(buffers));
-      }
-      else {
-        buffers.push(data, hash);
-        hash = hashFn(Buffer.concat(buffers));
-      }
+    const node = proof[i];
+    let data = null;
+    let isLeftNode = null;
+    if (typeof node === "string") {
+      data = tree.bufferify(node);
+      isLeftNode = true;
+    } else {
+      throw new Error("Expected node to be of type string or object");
+    }
+    const buffers = [];
+    if (Buffer.compare(hash, data) === -1) {
+      buffers.push(hash, data);
+      hash = hashFn(Buffer.concat(buffers));
+    } else {
+      buffers.push(data, hash);
+      hash = hashFn(Buffer.concat(buffers));
+    }
   }
-  return tree.bufferToHex(hash)
+  return tree.bufferToHex(hash);
 }
 
 const normalize = (order: Types.BaseOrder): Types.BaseOrder => {
@@ -326,11 +473,12 @@ const normalize = (order: Types.BaseOrder): Types.BaseOrder => {
   // - lowercase all strings
 
   return {
+    kind: order.kind,
     side: order.side,
     trader: lc(order.trader),
     matchingPolicy: lc(order.matchingPolicy),
     collection: lc(order.collection),
-    tokenId: n(order.tokenId),
+    tokenId: s(order.tokenId),
     nonce: s(order.nonce),
     amount: s(order.amount),
     paymentToken: lc(order.paymentToken),
