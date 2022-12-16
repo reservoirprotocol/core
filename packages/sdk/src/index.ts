b6b80a0874f21d9a82e245e76db17b753f5c05b3
George Roman <george.roman.99@gmail.com>
2022-12-16 15:02:47 +0100

    refactor: reorder imports and exports

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,40 +1,46 @@
 import * as Common from "./common";
+
+import * as RouterV5 from "./router/v5";
+import * as RouterV6 from "./router/v6";
+
+import * as Beeple from "./beeple";
+import * as BendDao from "./bend-dao";
 import * as Blur from "./blur";
+import * as CryptoArte from "./cryptoarte";
+import * as CryptoKitties from "./cryptokitties";
 import * as CryptoPunks from "./cryptopunks";
+import * as CryptoVoxels from "./cryptovoxels";
+import * as Decentraland from "./decentraland";
 import * as Element from "./element";
 import * as Forward from "./forward";
 import * as Foundation from "./foundation";
 import * as Infinity from "./infinity";
 import * as LooksRare from "./looks-rare";
+import * as Manifold from "./manifold";
+import * as NftTrader from "./nft-trader";
 import * as Nftx from "./nftx";
 import * as Nouns from "./nouns";
+import * as Okex from "./okex";
 import * as Quixotic from "./quixotic";
 import * as Rarible from "./rarible";
 import * as Seaport from "./seaport";
 import * as Sudoswap from "./sudoswap";
 import * as SuperRare from "./superrare";
+import * as TofuNft from "./tofunft";
 import * as Universe from "./universe";
 import * as WyvernV2 from "./wyvern-v2";
 import * as WyvernV23 from "./wyvern-v2.3";
 import * as X2Y2 from "./x2y2";
 import * as ZeroExV4 from "./zeroex-v4";
 import * as Zora from "./zora";
-import * as Manifold from "./manifold";
-import * as CryptoKitties from "./cryptokitties";
-import * as Beeple from "./beeple";
-import * as CryptoArte from "./cryptoarte";
-import * as CryptoVoxels from "./cryptovoxels";
-import * as TofuNft from "./tofunft";
-import * as BendDao from "./bend-dao";
-import * as Okex from "./okex";
-import * as NftTrader from "./nft-trader";
-import * as Decentraland from "./decentraland";
-
-import * as RouterV5 from "./router/v5";
-import * as RouterV6 from "./router/v6";
 
 export {
+  // Common
   Common,
+  // Routers
+  RouterV5,
+  RouterV6,
+  // Contracts / Protocols
   Beeple,
   BendDao,
   Blur,
@@ -46,8 +52,8 @@ export {
   Element,
   Forward,
   Foundation,
-  LooksRare,
   Infinity,
+  LooksRare,
   Manifold,
   NftTrader,
   Nftx,
@@ -65,6 +71,4 @@ export {
   X2Y2,
   ZeroExV4,
   Zora,
-  RouterV5,
-  RouterV6,
 };
