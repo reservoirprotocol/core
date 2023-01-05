83c5375ad773779de99c3ce4c09ac7f25593b1ea
George Roman <george.roman.99@gmail.com>
2023-01-05 12:57:45 +0100

    chore: update version

--- packages/sdk/src/seaport/addresses.ts
+++ packages/sdk/src/seaport/addresses.ts
@@ -2,44 +2,39 @@ import { ChainIdToAddress, Network } from "../utils";
 
 export const Exchange: ChainIdToAddress = {
   [Network.Ethereum]: "0x00000000006c3852cbef3e08e8df289169ede581",
-  [Network.EthereumRinkeby]: "0x00000000006c3852cbef3e08e8df289169ede581",
   [Network.EthereumGoerli]: "0x00000000006c3852cbef3e08e8df289169ede581",
   [Network.Optimism]: "0x00000000006c3852cbef3e08e8df289169ede581",
-  [Network.EthereumKovan]: "0x00000000006c3852cbef3e08e8df289169ede581",
-  [Network.OptimismKovan]: "0x00000000006c3852cbef3e08e8df289169ede581",
   [Network.Gnosis]: "0x00000000006c3852cbef3e08e8df289169ede581",
   [Network.Polygon]: "0x00000000006c3852cbef3e08e8df289169ede581",
+  [Network.PolygonMumbai]: "0x00000000006c3852cbef3e08e8df289169ede581",
   [Network.Arbitrum]: "0x00000000006c3852cbef3e08e8df289169ede581",
   [Network.AvalancheFuji]: "0x00000000006c3852cbef3e08e8df289169ede581",
   [Network.Avalanche]: "0x00000000006c3852cbef3e08e8df289169ede581",
-  [Network.PolygonMumbai]: "0x00000000006c3852cbef3e08e8df289169ede581",
 };
 
 export const ConduitController: ChainIdToAddress = {
   [Network.Ethereum]: "0x00000000f9490004c11cef243f5400493c00ad63",
-  [Network.EthereumRinkeby]: "0x00000000f9490004c11cef243f5400493c00ad63",
   [Network.EthereumGoerli]: "0x00000000f9490004c11cef243f5400493c00ad63",
   [Network.Optimism]: "0x00000000f9490004c11cef243f5400493c00ad63",
-  [Network.EthereumKovan]: "0x00000000f9490004c11cef243f5400493c00ad63",
-  [Network.OptimismKovan]: "0x00000000f9490004c11cef243f5400493c00ad63",
   [Network.Gnosis]: "0x00000000f9490004c11cef243f5400493c00ad63",
   [Network.Polygon]: "0x00000000f9490004c11cef243f5400493c00ad63",
+  [Network.PolygonMumbai]: "0x00000000f9490004c11cef243f5400493c00ad63",
   [Network.Arbitrum]: "0x00000000f9490004c11cef243f5400493c00ad63",
   [Network.AvalancheFuji]: "0x00000000f9490004c11cef243f5400493c00ad63",
   [Network.Avalanche]: "0x00000000f9490004c11cef243f5400493c00ad63",
-  [Network.PolygonMumbai]: "0x00000000f9490004c11cef243f5400493c00ad63",
 };
 
 export const PausableZone: ChainIdToAddress = {
   [Network.Ethereum]: "0x004c00500000ad104d7dbd00e3ae0a5c00560c00",
-  [Network.EthereumRinkeby]: "0x00000000e88fe2628ebc5da81d2b3cead633e89e",
   [Network.Polygon]: "0x004c00500000ad104d7dbd00e3ae0a5c00560c00",
+};
+
+export const CancelXZone: ChainIdToAddress = {
+  [Network.EthereumGoerli]: "0x601d58906d22ce2fabdfb112e15e515557aa191c",
 };
 
 export const OpenseaConduitKey: ChainIdToAddress = {
   [Network.Ethereum]:
-    "0x0000007b02230091a7ed01230072f7006a004d60a8d4e71d599b8104250f0000",
-  [Network.EthereumRinkeby]:
     "0x0000007b02230091a7ed01230072f7006a004d60a8d4e71d599b8104250f0000",
   [Network.EthereumGoerli]:
     "0x0000007b02230091a7ed01230072f7006a004d60a8d4e71d599b8104250f0000",
