63ca0d21e8709aa129c4228546aaba9e09f4957b
George Roman <george.roman.99@gmail.com>
2023-01-13 13:30:58 +0200

    fix: nftx pricing

---
+++ packages/sdk/src/nftx/helpers.ts
@@ -0,0 +1,112 @@
+import { Interface } from "@ethersproject/abi";
+import { Provider } from "@ethersproject/abstract-provider";
+import { BigNumber, BigNumberish } from "@ethersproject/bignumber";
+import { Contract } from "@ethersproject/contracts";
+import { parseEther } from "@ethersproject/units";
+
+import * as Common from "../common";
+import { bn } from "../utils";
+import * as Addresses from "./addresses";
+
+export const getPoolPrice = async (
+  vault: string,
+  amount: BigNumberish,
+  slippage: number,
+  provider: Provider
+) => {
+  let buyPrice: BigNumberish | null = null;
+  let sellPrice: BigNumberish | null = null;
+
+  const iface = new Interface([
+    "function getAmountsOut(uint amountIn, address[] memory path) view returns (uint[] memory amounts)",
+    "function getAmountsIn(uint amountOut, address[] memory path) view returns (uint[] memory amounts)",
+  ]);
+
+  const chainId = await provider.getNetwork().then((n) => n.chainId);
+
+  const weth = Common.Addresses.Weth[chainId];
+  const sushiRouter = new Contract(
+    Addresses.SushiRouter[chainId],
+    iface,
+    provider
+  );
+
+  amount = parseEther(amount.toString());
+
+  const fees = await getPoolFees(vault, provider);
+  const unit = parseEther("1");
+
+  try {
+    const path = [weth, vault];
+    const amounts = await sushiRouter.getAmountsIn(
+      amount.add(amount.mul(fees.redeemFee).div(unit)),
+      path
+    );
+    buyPrice = amounts[0].div(amount);
+    if (slippage) {
+      buyPrice = bn(buyPrice!).add(bn(buyPrice!).mul(slippage).div(10000));
+    }
+  } catch {
+    // Skip errors
+  }
+
+  try {
+    const path = [vault, weth];
+    const amounts = await sushiRouter.getAmountsOut(amount, path);
+    sellPrice = amounts[1].div(amount);
+    if (slippage) {
+      sellPrice = bn(sellPrice!).sub(bn(sellPrice!).mul(slippage).div(10000));
+    }
+  } catch {
+    // Skip errors
+  }
+
+  return {
+    amount,
+    currency: weth,
+    bps: {
+      sell: bn(fees.mintFee).div("100000000000000"),
+      buy: bn(fees.redeemFee).div("100000000000000"),
+    },
+    price: {
+      sell: sellPrice?.toString(),
+      buy: buyPrice?.toString(),
+    },
+  };
+};
+
+export const getPoolNFTs = async (vault: string, provider: Provider) => {
+  const tokenIds: string[] = [];
+  const iface = new Interface([
+    "function allHoldings() external view returns (uint256[] memory)",
+  ]);
+
+  const factory = new Contract(vault, iface, provider);
+  try {
+    const holdingNFTs = await factory.allHoldings();
+    holdingNFTs.forEach((c: BigNumber) => {
+      tokenIds.push(c.toString());
+    });
+  } catch {
+    // Skip errors
+  }
+  return tokenIds;
+};
+
+export const getPoolFees = async (address: string, provider: Provider) => {
+  const iface = new Interface([
+    "function mintFee() public view returns (uint256)",
+    "function targetRedeemFee() public view returns (uint256)",
+  ]);
+
+  const vault = new Contract(address, iface, provider);
+  const [mintFee, redeemFee] = await Promise.all([
+    vault.mintFee(),
+    vault.targetRedeemFee(),
+  ]);
+
+  return {
+    mintFee: mintFee.toString(),
+    redeemFee: redeemFee.toString(),
+  };
+};
--- packages/sdk/src/nftx/index.ts
+++ packages/sdk/src/nftx/index.ts
@@ -1,6 +1,6 @@
 import * as Addresses from "./addresses";
 import { Order } from "./order";
-import * as Helpers from "./helper";
+import * as Helpers from "./helpers";
 import * as Types from "./types";
 
 export { Addresses, Order, Helpers, Types };
