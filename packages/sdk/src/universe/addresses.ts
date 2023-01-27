3e6bf85e377b6c148039cb7acbc47f3c1961f138
George Roman <george.roman.99@gmail.com>
2023-01-27 15:50:16 +0200

    fix: build

--- packages/sdk/src/universe/addresses.ts
+++ packages/sdk/src/universe/addresses.ts
@@ -2,6 +2,5 @@ import { ChainIdToAddress, Network } from "../utils";
 
 export const Exchange: ChainIdToAddress = {
   [Network.Ethereum]: "0xb720279a6fc982721771427aa3b6b3a135f81de3",
-  [Network.EthereumRinkeby]: "0xe441c394f917d5a90019c76897458257801ce132",
-  [Network.EthereumGoerli]: "0xde8c8ff3835c5e5cae88b5b403ef56b9243381b1"
+  [Network.EthereumGoerli]: "0xde8c8ff3835c5e5cae88b5b403ef56b9243381b1",
 };
