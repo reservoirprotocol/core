0d8c80a09c4c019c9f19cef83735a7d68722fac7
Boris Bonin <boris.bonin@limechain.tech>
2023-02-08 14:06:38 +0200

    chore: add zeroex v2 eth address

--- packages/sdk/src/zeroex-v2/addresses.ts
+++ packages/sdk/src/zeroex-v2/addresses.ts
@@ -3,3 +3,7 @@ import { ChainIdToAddress, Network } from "../utils";
 export const Exchange: ChainIdToAddress = {
   [Network.Ethereum]: "0x080bf510fcbf18b91105470639e9561022937712",
 };
+
+export const Eth: ChainIdToAddress = {
+  [Network.Ethereum]: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+};
